7d5ebe145b2f0b19dc6069ef8099da27232d098a
DonnellWashington <wasnelly4@gmail.com>
2025-03-28 08:31:01 -0400

    Main stuff

--- civ.py
+++ civ.py
@@ -1,2 +1,8 @@
-print("Hellow world")
+def main():
+    print("Welcome to the civilization game")
+    print("I realised I cant call it civilization cause thats the name of another game soo")
+    print("We will call it")
 
+
+if __name__ == '__main__':
+    main()
